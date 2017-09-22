9506af48baf8b2c008b0c64b72ac2d2a120fc669
Pedro G. Dias <pedro@digitaldias.com>
2017-09-21 17:33:30 -0700

    Introduce a baseclass for all unit-tests in a separate testproject

---
+++ BotMessageRoutingTests/TestsFor.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using StructureMap.AutoMocking;
+using StructureMap.AutoMocking.Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotMessageRoutingTests
+{
+    public class TestsFor<TEntity> where TEntity : class
+    {
+        public TEntity Instance { get; set; }
+        public MoqAutoMocker<TEntity> AutoMock { get; set; }
+
+        public TestsFor()
+        {
+            AutoMock = new MoqAutoMocker<TEntity>();
+
+            Instance = AutoMock.ClassUnderTest;
+        }
+
+        public Mock GetMockFor<TContract>() where TContract : class
+        {
+            return Mock.Get(AutoMock.Get<TContract>());
+        }
+    }
+}
